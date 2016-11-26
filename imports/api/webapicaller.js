aed7934b569c08f3309d5a21cfed9c4a46705ae6
mertcanat <mcanat@ku.edu.tr>
2016-11-26 15:21:42 +0100

    Resolved song detials in querySpotify

--- imports/api/webapicaller.js
+++ imports/api/webapicaller.js
@@ -11,10 +11,18 @@ if (Meteor.isServer) {
             var results = Meteor.http.call("GET", "https://api.spotify.com/v1/search?query=abba&offset=0&limit=20&type=track");
             //unique ID
             //first five results, name of the song, artist
-            //var items = [results.items[0],results.items[1],results.items[2],results.items[3],results.items[4]];
-            var items = results.data.tracks.items[0];
-            console.log(items);
-            console.log("I am here.");
+
+            //var items = results.data.tracks.items[0];
+
+
+            var length = 5;
+            for (i = 0; i < length; i++) {
+                var songname = results.data.tracks.items[i].name;
+                var artist = results.data.tracks.items[i].artists[0].name;
+                var songid = results.data.tracks.items[i].id;
+            }
+
+            //console.log("I am here.");
         }
     });
 }
