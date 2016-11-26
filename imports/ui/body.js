6b20294af52c588d17958ac660d8a748c0b9f064
Jakob <jakobhedlund@hotmail.com>
2016-11-26 21:00:58 +0100

    change of UI

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -15,6 +15,18 @@ import './playlistitem.html'
 Template.body.onCreated(function bodyOnCreated() {
     this.state = new ReactiveDict();
     Meteor.subscribe('tracks');
+
+    var song = {
+        songname: 'Chantaje',
+        artist: 'Shakira',
+        thumbnail: {
+            height: 640,
+            url: 'https://i.scdn.co/image/c422311a3e1eba6fa97fc2286be73cc124149dcb',
+            width: 640
+        },
+        songid: '1WniHvhq9zTkny0WvGXX8o'
+    };
+    Session.set("tempTracks", [ song, song ]);
 });
 
 Template.body.helpers({
@@ -41,7 +53,7 @@ Template.body.events({
         const text = target.text.value;
 
         // Insert a task into the collection
-        var tracks = Meteor.call('querySpotify', text, function(err, data) {
+        var tracks = Meteor.call('querySpotify', text, function (err, data) {
             if (err) {
                 console.log(err);
             }
@@ -50,7 +62,6 @@ Template.body.events({
 
 
         });
-
 
 
         // Clear form
