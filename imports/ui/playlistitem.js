df813b7ee3c73e8f5f8aaffb81468c902e526ff4
Jakob <jakobhedlund@hotmail.com>
2016-11-27 11:38:07 +0100

    UI stuff and device button

--- imports/ui/playlistitem.js
+++ imports/ui/playlistitem.js
@@ -1,2 +1,8 @@
 import './playlistitem.html'
 
+Template.playlistitem.events({
+    'click .cd-timeline-thumbnail'(event) {
+        event.preventDefault();
+        $(".horizontal-align").addClass("translateRight");
+    }
+});
